24574ebb2b8cec463dcbf16de1b48b3cd14c5bbe
JamBur05 <james3906@gmail.com>
2024-12-01 20:39:57 +0000

    TIming for 1.2

--- day1/1.2.py
+++ day1/1.2.py
@@ -1,3 +1,6 @@
+import time
+start_time = time.perf_counter()
+
 file = open("input.txt", "r")
 
 list1 = []
@@ -6,16 +9,23 @@ total = 0
 
 for x in file:
     data = x.split("   ")
-    list1.append(data[0])
-    list2.append(data[1])
+    list1.append(int(data[0]))
+    list2.append(int(data[1]))
 
+input_reading_time = (time.perf_counter() - start_time) * 1_000_000  # Convert to microseconds
+print(f"Time for reading and processing input: {input_reading_time:.2f} microseconds")
+
+start_time = time.perf_counter()
 
 for i in range(len(list1)):
     counter = 0
     for j in range(len(list2)):
-        if(int(list1[i]) == int(list2[j])):
+        if(list1[i] == list2[j]):
             counter += 1
         
-    total += int(list1[i]) * counter
+    total += list1[i] * counter
+
+computation_time = (time.perf_counter() - start_time) * 1_000_000  # Convert to microseconds
+print(f"Time for computations: {computation_time:.2f} microseconds")
 
 print(total)
