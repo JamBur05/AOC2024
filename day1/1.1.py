194cf8226c53f51b47a43c67d2c4a22a54fafd48
JamBur05 <james3906@gmail.com>
2024-12-01 20:49:33 +0000

    Timing 1.1

--- day1/1.1.py
+++ day1/1.1.py
@@ -16,3 +16,17 @@ for i in range(len(list1)):
     total += abs(list1[i] - list2[i])
 
 print(total)
+
+'''
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 0
+Milliseconds      : 52
+Ticks             : 525550
+TotalDays         : 6.08275462962963E-07
+TotalHours        : 1.45986111111111E-05
+TotalMinutes      : 0.000875916666666667
+TotalSeconds      : 0.052555
+TotalMilliseconds : 52.555
+'''
